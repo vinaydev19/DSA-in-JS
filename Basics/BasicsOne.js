a8adb4e92f3cef1c0853f885baf4bc766f56037f
vinay1945 <vinaykumarporandla19@gmail.com>
2025-04-24 15:20:03 +0530

    restart dsa and completed basic One

---
+++ Basics/BasicsOne.js
@@ -0,0 +1,62 @@
+/*let i = 11;
+i = i++ + ++i;
+console.log(i);
+*/
+
+
+/*
+let a =11
+let b =12
+let c = a + b + a++ + b++ + ++a + ++b
+console.log("a", a); // 13
+console.log("b", b); // 14
+console.log("c", c); //73
+ */
+
+/*
+let b = true
+++b
+console.log(b);
+*/
+
+/*
+let a = 11++ 
+console.log(a);
+// SyntaxError: Invalid left-hand side expression in postfix operation 
+*/
+
+
+// question
+
+// Q.calculate area and perimeter of rectangle
+// area
+/*
+let a = 5
+let b = 7
+console.log(a*b);
+*/
+
+// perimeter
+/*
+let a = 5
+let b = 7
+console.log(2* (a + b));
+*/
+
+// Q.Generate OTP
+// console.log(Math.trunc(Math.random() * 9000) + 1000);
+
+// Qarea of triangle by heron's formula
+/*
+let a =5
+let b= 4
+let c = 3
+let s = (a+b+c)/2
+console.log(Math.sqrt(s * (s-a) * (s-b) * (s-c)));
+*/
+
+// Q.Circumference of circle
+/*
+let r = 12
+console.log(2*Math.PI*r);
+*/
