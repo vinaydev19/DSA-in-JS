727a64f93422b30e272b771e1129fb777a62d9cd
vinay1945 <vinaykumarporandla19@gmail.com>
2025-09-22 07:58:34 +0530

    dsa: Day 1 - solved array problems (min/max, reverse, rotate)

--- DSA/Array/ArrayMastery.js
+++ DSA/Array/ArrayMastery.js
@@ -36,7 +36,7 @@ function FindMaxAndMin(arr) {
 }
 
 let arr = [6, 5, 10, 12, 1]
-console.log(FindMaxAndMin(arr));
+console.log("Max and Min is: ", FindMaxAndMin(arr));
 
 
 
@@ -55,7 +55,7 @@ function reverseArray(arr) {
     return arr
 }
 let arr1 = [1, 2, 3, 4, 6]
-console.log(reverseArray(arr1));
+console.log("Reversed Array is using temp arr: ", reverseArray(arr1));
 
 
 // Reverse an Array (in-place) - way use two pointer
@@ -72,81 +72,131 @@ function reverseArray1(arr) {
     return arr
 }
 let arr2 = [1, 2, 3, 4, 6]
-console.log(reverseArray1(arr2));
+console.log("Reversed Array is using two pointer: ", reverseArray1(arr2));
 
 
-// Rotate Array by K steps (left & right)
+// Rotate Array (left/right by 1)
 
-/*
-// this left rotation
-let k = Number(prompt("enter a k number"))
-let arr = [1, 2, 3, 4, 5]
-k = k % arr.length
-for (let i = 0; i < k; i++) {
-    temp = arr[0]
-    for (let j = 0; j < arr.length - 1; j++) {
-        arr[j] = arr[j + 1]
+function rotateArrayLeftByOne(arr) {
+    let temp = arr[0]
+
+    for (let i = 1; i < arr.length; i++) {
+        arr[i - 1] = arr[i]
     }
+
     arr[arr.length - 1] = temp
+
+    return arr
 }
-console.log(arr);
-*/
 
+let arr3 = [1, 2, 3, 4, 5]
+console.log("Array after left rotation by one: ", rotateArrayLeftByOne(arr3));
 
-/*
-// this right rotation
-let k = Number(prompt("enter a k number"))
-let arr = [1, 2, 3, 4, 5]
-k = k % arr.length
-for (let i = 0; i < k; i++) {
-    temp = arr[arr.length - 1]
-    for (let j = arr.length - 1; j > 0; j--) {
-        arr[j] = arr[j - 1]
+function rotateArrayRightByOne(arr) {
+    let temp = arr[arr.length - 1]
+
+    for (let i = arr.length - 2; i >= 0; i--) {
+        arr[i + 1] = arr[i]
     }
+
     arr[0] = temp
+
+    return arr
 }
-console.log(arr);
-*/
+
+let arr4 = [1, 2, 3, 4, 5]
+console.log("Array after right rotation by one: ", rotateArrayRightByOne(arr4));
 
 
-// Q:- Left Rotation of an Array using Temporary Array
-/*
-let arr = [1, 2, 3, 4, 5]
-let temp = new Array(arr.length)
-let k = Number(prompt("enter a k number"))
-k = k % arr.length
-for (let i = 0; i < arr.length; i++) {
-    temp[i] = arr[(i + k) % arr.length]
+// Rotate Array (left/right by k) - way 1 (using loop k times)
+
+function rotateArrayLeftByKLoopKTimes(arr, k) {
+    k = k % arr.length
+    for (let i = 0; i < k; i++) {
+        let temp = arr[0]
+
+        for (let j = 0; j < arr.length - 1; j++) {
+            arr[j] = arr[j + 1]
+        }
+
+        arr[arr.length - 1] = temp
+    }
+
+    return arr
 }
-console.log(temp);
-*/
+
+let arr5 = [1, 2, 3, 4, 5]
+let k1 = 2
+console.log("Array after left rotation by k using loop k times: ", rotateArrayLeftByKLoopKTimes(arr5, k1));
 
 
+function rotateArrayRightByKLoopKTimes(arr, k) {
+    k = k % arr.length
+    for (let i = 0; i < k; i++) {
+        let temp = arr[arr.length - 1]
 
-// right
-/*
-let arr = [1, 2, 3, 4, 5]
-let temp = new Array(arr.length)
-let k = 2
-k = k % arr.length
-for (let i = 0; i < arr.length; i++) {
-    temp[i] = arr[((arr.length - k) + i) % arr.length]
+        for (let j = arr.length - 1; j > 0; j--) {
+            arr[j] = arr[j - 1]
+        }
+
+        arr[0] = temp
+    }
+
+    return arr
 }
-console.log(temp);
-*/
+
+let arr6 = [1, 2, 3, 4, 5]
+let k2 = 2
+console.log("Array after right rotation by k using loop k times: ", rotateArrayRightByKLoopKTimes(arr6, k2));
 
 
+// Rotate Array (left/right by k) - way 2 (using temp array)
 
-//Q:- Reversal Algorithm for Array left Rotation
-/*
-let arr = [1, 2, 3, 4, 5]
-let k = 2
-k = k % arr.length
-reverse(0, k - 1)
-reverse(k, arr.length - 1)
-reverse(0, arr.length - 1)
-console.log(arr);
-function reverse(i, j) {
+function rotateArrayLeftByKUsingTempArray(arr, k) {
+    k = k % arr.length
+    let temp = new Array(arr.length)
+
+    for (let i = 0; i < arr.length; i++) {
+        temp[i] = arr[(i + k) % arr.length]
+    }
+
+    // this options if don't want to return new array and want to modify original array
+    // for (let i = 0; i < arr.length; i++) {
+    //     arr[i] = temp[i]
+    // }
+
+    return temp
+}
+
+let arr7 = [1, 2, 3, 4, 5]
+let k3 = 2
+console.log("Array after left rotation by k using temp array: ", rotateArrayLeftByKUsingTempArray(arr7, k3));
+
+
+function rotateArrayRightByKUsingTempArray(arr, k) {
+    k = k % arr.length
+    let temp = new Array(arr.length)
+
+    for (let i = 0; i < arr.length; i++) {
+        temp[i] = arr[((arr.length - k) + i) % arr.length]
+    }
+
+    // this options if don't want to return new array and want to modify original array
+    // for (let i = 0; i < arr.length; i++) {
+    //     arr[i] = temp[i]
+    // }
+
+    return temp
+}
+
+let arr8 = [1, 2, 3, 4, 5]
+let k4 = 2
+console.log("Array after right rotation by k using temp array: ", rotateArrayRightByKUsingTempArray(arr8, k4));
+
+
+// Rotate Array (left/right by k) - way 3 (using reversal algorithm)
+
+function reverse(arr, i, j) {
     while (i < j) {
         let temp = arr[i]
         arr[i] = arr[j]
@@ -155,25 +205,32 @@ function reverse(i, j) {
         j--
     }
 }
-*/
 
-// right Rotation
-/*
-let arr = [1, 2, 3, 4, 5]
-let k = 3
-k = k % arr.length
+function rotateArrayLeftByKUsingReversalAlgorithm(arr, k) {
+    k = k % arr.length
 
-reverse(0, arr.length - 1)
-reverse(0, k - 1)
-reverse(k, arr.length - 1)
-console.log(arr);
-function reverse(i, j) {
-    while (i < j) {
-        let temp = arr[i]
-        arr[i] = arr[j]
-        arr[j] = temp
-        i++
-        j--
-    }
+    reverse(arr, 0, k - 1)
+    reverse(arr, k, arr.length - 1)
+    reverse(arr, 0, arr.length - 1)
+
+    return arr
 }
-*/
+
+let arr9 = [1, 2, 3, 4, 5]
+let k5 = 2
+console.log("Array after left rotation by k using reversal algorithm: ", rotateArrayLeftByKUsingReversalAlgorithm(arr9, k5));
+
+
+function rotateArrayRightByKUsingReversalAlgorithm(arr, k) {
+    k = k % arr.length
+
+    reverse(arr, 0, arr.length - 1)
+    reverse(arr, 0, k - 1)
+    reverse(arr, k, arr.length - 1)
+
+    return arr
+}
+
+let arr10 = [1, 2, 3, 4, 5]
+let k6 = 2
+console.log("Array after right rotation by k using reversal algorithm: ", rotateArrayRightByKUsingReversalAlgorithm(arr10, k6));
